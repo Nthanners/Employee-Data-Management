0b452110c41d662847da0ab24504b00ec6ebade8
Nthanners <130081631+Nthanners@users.noreply.github.com>
2023-07-02 17:38:40 +0800

    added list and method to the main code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -7,17 +8,19 @@ class Program
         int choice = 0;
         int employeeCount = 0;
         string[,] employees = new string[10, 4];
+        List<string> employeeList = new List<string>();
 
         do
         {
-            Console.WriteLine("*********************");
-            Console.WriteLine("**     MENU        **");
-            Console.WriteLine("*********************");
+            Console.WriteLine("*******************");
+            Console.WriteLine("*     MENU        *");
+            Console.WriteLine("*******************");
             Console.WriteLine("1. Enter Employee Data:");
             Console.WriteLine("2. Sort Employee Data by Start Date");
             Console.WriteLine("3. Display Employee Data");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine("Enter your Choice (1-4):");
+            Console.WriteLine("4. Display Employee List");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("Enter your Choice (1-5):");
             choice = int.Parse(Console.ReadLine());
 
             switch (choice)
@@ -78,6 +81,10 @@ class Program
                     break;
 
                 case 4:
+                    DisplayEmployeeList(employeeList);
+                    break;
+
+                case 5:
                     Console.WriteLine("Exiting Program...");
                     break;
 
@@ -86,7 +93,7 @@ class Program
                     break;
             }
 
-        } while (choice != 4);
+        } while (choice != 5);
     }
 
     static void DisplayEmployeeData(string[,] employees, int count)
@@ -95,8 +102,19 @@ class Program
         Console.WriteLine("Name Department Salary StartDate");
         for (int i = 0; i < count; i++)
         {
-            Console.WriteLine("{0} {1} {2} {3}", employees[i, 0], employees[i, 1], employees[i, 2], employees[i, 3]);
+                        Console.WriteLine("{0} {1} {2} {3}", employees[i, 0], employees[i, 1], employees[i, 2], employees[i, 3]);
         }
         Console.WriteLine();
     }
+        
+    static void DisplayEmployeeList(List<string> employeeList)
+    {
+        Console.WriteLine("Employee List:");
+        foreach (string employee in employeeList)
+        {
+            Console.WriteLine(employee);
+        }
+        Console.WriteLine();
+    }
+    
 }
