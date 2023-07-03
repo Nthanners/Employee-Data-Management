bd2c6a51641fc53ce799e29fbb94e2ae7dd616f3
Nthanners <130081631+Nthanners@users.noreply.github.com>
2023-07-03 11:14:50 +0800

    Updated into 2 seperate classes

---
+++ EmployeeManagement.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+
+class EmployeeManagement
+{
+    private string[,] employees = new string[10, 4];
+    private List<string> employeeList = new List<string>();
+    private int employeeCount = 0;
+
+    public void RunEmployeeManagementSystem()
+    {
+        int choice = 0;
+
+        do
+        {
+            Console.WriteLine("*******************");
+            Console.WriteLine("*     MENU        *");
+            Console.WriteLine("*******************");
+            Console.WriteLine("1. Enter Employee Data:");
+            Console.WriteLine("2. Sort Employee Data by Start Date");
+            Console.WriteLine("3. Display Employee Data");
+            Console.WriteLine("4. Display Employee List");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("Enter your Choice (1-5):");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    EnterEmployeeData();
+                    break;
+
+                case 2:
+                    SortEmployeeDataByStartDate();
+                    Console.WriteLine("** Employee Data Sorted by Start Date! **\n");
+                    break;
+
+                case 3:
+                    DisplayEmployeeData();
+                    break;
+
+                case 4:
+                    DisplayEmployeeList();
+                    break;
+
+                case 5:
+                    Console.WriteLine("Exiting Program...");
+                    break;
+
+                default:
+                    Console.WriteLine("** Invalid Choice! Please Choose A Valid Option. **\n");
+                    break;
+            }
+
+        } while (choice != 5);
+    }
+
+    private void EnterEmployeeData()
+    {
+        Console.WriteLine("** Enter Data for the Employee **\n");
+        if (employeeCount < 10)
+        {
+            Console.WriteLine("Enter Data for Employee {0}:", employeeCount + 1);
+            Console.WriteLine("Employee Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Department:");
+            string department = Console.ReadLine();
+            Console.WriteLine("Salary:");
+            string salary = Console.ReadLine();
+            Console.WriteLine("Start date (DD/MM/YYYY): ");
+            string startDate = Console.ReadLine();
+
+            string employeeData = $"{name}, {department}, {salary}, {startDate}";
+            employeeList.Add(employeeData);
+
+            employeeCount++;
+            Console.WriteLine("** Employee Data Entered Successfully! **\n");
+        }
+        else
+        {
+            Console.WriteLine("** Maximum number of employees reached! **\n");
+        }
+    }
+
+    private void SortEmployeeDataByStartDate()
+    {
+        for (int i = 0; i < employeeCount; i++)
+        {
+            for (int j = i + 1; j < employeeCount; j++)
+            {
+                DateTime date1 = DateTime.Parse(employees[i, 3]);
+                DateTime date2 = DateTime.Parse(employees[j, 3]);
+                if (date1 > date2)
+                {
+                    string tempName = employees[i, 0];
+                    string tempDept = employees[i, 1];
+                    string tempSalary = employees[i, 2];
+                    string tempDate = employees[i, 3];
+
+                    employees[i, 0] = employees[j, 0];
+                    employees[i, 1] = employees[j, 1];
+                    employees[i, 2] = employees[j, 2];
+                    employees[i, 3] = employees[j, 3];
+
+                    employees[j, 0] = tempName;
+                    employees[j, 1] = tempDept;
+                    employees[j, 2] = tempSalary;
+                    employees[j, 3] = tempDate;
+                }
+            }
+        }
+    }
+
+    private void DisplayEmployeeData()
+    {
+        Console.WriteLine("Employee Data:");
+        Console.WriteLine("Name Department Salary StartDate");
+        for (int i = 0; i < employeeCount; i++)
+        {
+            Console.WriteLine("{0} {1} {2} {3}", employees[i, 0], employees[i, 1], employees[i, 2], employees[i, 3]);
+        }
+        Console.WriteLine();
+    }
+
+    private void DisplayEmployeeList()
+    {
+        Console.WriteLine("Employee List:");
+        foreach (string employee in employeeList)
+        {
+            string[] employeeData = employee.Split(',');
+            string name = employeeData[0].Trim();
+            Console.WriteLine(name);
+        }
+    Console.WriteLine();
+    }
+}
